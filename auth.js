140beb7c87aab530b49b27e2ebddd7e5be9c0559
LeslieSoriano <126890910+LeslieSoriano@users.noreply.github.com>
2026-03-06 14:18:13 -0600

    Add auth.js to pages and update redirects
    Insert authentication script tags into multiple atencionpublico pages (quejas/index.html, tramites/calculador/index.html, tramites/catalogos.html, tramites/diagrama_flujo_tramites.html, tramites/listados.html) so they load auth.js. Update auth.js to include additional window.location.href redirect targets (several proceses pages such as parcialidades, tramites listados, areatecnica, cobranza, recfinancier, extras, USCA). Also apply a minor whitespace change in indexmenu.html. These changes enable consistent client-side authentication/redirect behavior across the affected pages.

--- auth.js
+++ auth.js
@@ -5,6 +5,19 @@
   if (!usuario || !rol) {
     window.location.href = "https://lesliesoriano.github.io/procesos/indexmenu.html";
     window.location.href = "https://lesliesoriano.github.io/procesos/atencionpublico/tramites/tramites_2026.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/atencionpublico/parcialidades/index.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/atencionpublico/parcialidades/calculo/index.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/atencionpublico/tramites/listados.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/areatecnica/procesorden.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/areatecnica/pipas.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/cobranza/conceptosduplicados.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/cobranza/conceptosduplicados2archivos.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/recfinancier/iva.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/recfinancier/calculadordv.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/recfinancier/conciliacion/index.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/extras/separadorexcel.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/USCA/diagrama.html";
+    window.location.href = "https://lesliesoriano.github.io/procesos/recfinancier/calculadordv.html";
     return;
   }
 
